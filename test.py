a1ef7fce514f9c334f80b3f5c712ec65711ab05f
ilona <ilonakvy@gmail.com>
2023-05-07 04:17:03 +0300

    №1,2

---
+++ test.py
@@ -0,0 +1,52 @@
+import random
+
+# Miller Rabin
+
+
+def euclidean_algorithm(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def miller_rabin(n, k):
+    if n <= 2:
+        return n == 2
+    r = 0
+    d = n - 1
+    while d % 2 == 0:
+        r += 1
+        d //= 2
+    for i in range(k):
+        a = random.randint(2, n - 1)
+        if euclidean_algorithm(a, n) != 1:
+            return False
+        x = pow(a, d, n)
+        if x == 1 or x == n - 1:
+            continue
+        for j in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
+# Algorithm 2
+
+
+def factorize(n):
+    factors = []
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+print(factorize(156))
