d87a7cbfb63a1bba0446d11ac40e40de99bb1fe3
Juuzou Suzuya <bitweal2@gmail.com>
2023-05-19 00:22:10 +0300

    v1.0.3

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 from Pollard import pollard
 from Brillhart_Morrison import brillhart_morrison
 
-factorize_numvers = []
+factorize_numbers = []
 
 def task_1(n):
     print('Тест на простоту Мiллера-Рабiна')
     k = int(input('Enter k: '))
     if miller_rabin(n,k):
         print(f'Число {n} є простим')
-        factorize_numvers.append(n)
+        factorize_numbers.append(n)
     else:
         print('Число є складеним переходимо до наступного кроку')
         return task_2(n)
@@ -18,8 +18,8 @@ def task_2(n):
     print('метод пробних дiлень')
     a = factorize(n)
     if len(a) > 0:
-        factorize_numvers.append(a[0])
-        print('дiльник a знайдено, повертаємось до попереднього кроку з числом', n//a[0])
+        factorize_numbers.append(a[0])
+        print('дiльник a знайдено, повертаємось до попереднього кроку з числом ', n//a[0])
         return task_1(n//a[0])
     else:
         print('дiльник не знайдено, переходимо до наступного кроку')
@@ -29,8 +29,8 @@ def task_3(n):
     print('ρ-метод Полларда ')
     a = pollard(n)
     if a != n:
-        factorize_numvers.append(a)
-        print('дiльник a знайдено, переходимо до наступного кроку', n//a)
+        factorize_numbers.append(a)
+        print('дiльник a знайдено, переходимо до наступного кроку з числом ', n//a)
         return task_4(n//a)
     else:
         return task_5(n)
@@ -40,7 +40,7 @@ def task_4(n):
     k = int(input('Enter k: '))
     if miller_rabin(n,k):
         print(f'Число {n} є простим')
-        factorize_numvers.append(n)
+        factorize_numbers.append(n)
     else:
         print('Число є складеним переходимо до наступного кроку')
         return task_5(n)
@@ -51,9 +51,9 @@ def task_5(n):
     if a == -1:
         return 'я не можу знайти канонiчний розклад числа :(.'
     else:
-        factorize_numvers.append(a[0])
+        factorize_numbers.append(a[0])
         return task_4(n//a[0])
 
 n = int(input('Enter n: '))
 task_1(n)
-print('Канонiчний розклад числа: ', factorize_numvers)
+print('Канонiчний розклад числа: ', factorize_numbers)
